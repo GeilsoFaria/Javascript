4865b9e31cc81fb3a4974cce9d52599ec1831e55
GeilsoF <geilsorodriguesgf@gmail.com>
2024-01-05 10:41:33 -0400

    condicoes JS

---
+++ aula006/ex008.js
@@ -0,0 +1,10 @@
+var velocidade = 60.5
+
+if(velocidade <= 60.5){
+    console.log(`Velocidade de ${velocidade} km/h. Dentro do permitido`)
+}
+else{
+    console.log(`Velocidade de ${velocidade} km/h. Fora do permitido`)
+    console.log('Multado!')
+}
+console.log('Use cinto de segurança')
---
+++ aula006/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+
+if(pais == 'Brasil'){
+    console.log('Seu erro é achar que está na América, aqui é o Brasil!')
+}
+else{
+    console.log('Realmente vc não está no Brasil. Tu mora em '+ pais)
+}
