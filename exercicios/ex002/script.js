a88a573e6d47a88ae156d1bd9bea08cd1e68381a
GeilsoF <geilsorodriguesgf@gmail.com>
2024-02-08 16:04:28 -0400

    implementei checar()

--- exercicios/ex002/script.js
+++ exercicios/ex002/script.js
@@ -1,23 +1,51 @@
-var ano = document.getElementById('idAno')
+function checar() {
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+    var anoForm = document.getElementById('idAno')
+    var divResultado = document.getElementById('idResultado')
 
-var sexo = document.getElementsByName('sexo')
-var div = document.getElementById('idResultado')
+    /*Pequena validação do ano*/
+    if (anoForm.value.length == 0 || Number(anoForm.value) > anoAtual) {
+        window.alert('Anos de Nascimento Inválido')
+    } else {
+        var sexo = document.getElementsByName('sexo')
+        var idade = anoAtual - Number(anoForm.value)
+        var genero = ''
+        var img = document.createElement('img')//Criar imagem dinamicamente
+        divResultado.style.textAlign = 'center'
+        img.setAttribute('id', 'foto')
+        divResultado.innerHTML =''
 
-function checar()
-{
-    var anoValor = parseInt(ano.value)
-    if(sexo[0].checked){//Feminino
-        if(anoValor > 2014){
-            div.innerHTML=''
-            var imagem = document.createElement('img')
-            imagem.src= '../imagens/crianca-menina.png'
-            div.appendChild(imagem)
-            
+        if (sexo[0].checked) {//Feminino
+            genero = 'Mulher'
+            if (idade > 0 && idade < 10) {
+                img.setAttribute('src','crianca-menina.png.png')
+            }
+            else if (idade < 18) {
+                img.setAttribute('src','adolescente-mulher.png')
+            } else if (idade < 60) {
+                img.setAttribute('src','adulto-mulher.png')
+            }
+            else {
+                img.setAttribute('src','idoso-mulher.png')
+            }
+        }
+        else if (sexo[1].checked) {//Masculino
+            genero = 'Homem'
+            if (idade > 0 && idade < 10) {
+                img.setAttribute('src','crianca-menino.png')
+            }
+            else if (idade < 18) {
+                img.setAttribute('src','adolescente-homem.png')
+            } else if (idade < 60) {
+                img.setAttribute('src','adulto-homem.png')
+            }
+            else {
+                img.setAttribute('src','idoso-homem.png')
+            }
         }
         
-    }
-    if(sexo[1].checked){
-        window.alert('Masculino')
-
-    }
+        divResultado.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        divResultado.appendChild(img)  
+    }  
 }
