bfedb10685b87c967f0aa5aff553c424b32a174d
GeilsoF <geilsorodriguesgf@gmail.com>
2024-02-09 09:00:09 -0400

    inicio repeticao

---
+++ aula008/ambiente.js
@@ -0,0 +1,13 @@
+var i = 0
+//Teste lógico no início da estrutura de repetição
+while(i<6){
+    i++
+    console.log(`Passo ${i}`)
+}
+console.log('-------')
+//Teste lógico no final da estrutura de repetição
+var j = 0
+do {
+    j++
+    console.log(`Passo ${j}`)
+} while (j < 6);
