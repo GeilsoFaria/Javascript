ce3cfa0abb4b50629985fe11d8ae7fde40b2d72c
GeilsoF <geilsorodriguesgf@gmail.com>
2024-02-13 16:52:07 -0400

    implementei contagem

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,24 +1,34 @@
 function contar(){
-    var divResultado = document.getElementById('idResposta')
-    var formInicio = document.getElementsByName('nomeInicio')[0].value
-    var formFim = document.getElementsByName('nomeFim')[0].value
-    var formPasso = document.getElementsByName('nomePasso')[0].value
-   
+    var divResultado = document.getElementById('idResposta')//Recebe a div de exibicao resultado
+    // var divAviso = document.getElementById('idAviso')
+    var formInicio = document.getElementById('inicio')
+    var formFim = document.getElementById('fim')
+    var formPasso = document.getElementById('passo')
 
-  
-
-    if( Number(formFim) > Number(formInicio)){
-        divResultado.innerHTML = ''
-        var inicio = Number(formInicio)
-        var fim = Number(formFim)
-        var passo= Number(formPasso)
-        // window.alert(`${inicio}, ${fim}, ${passo}`)
-        
-        // for(inicio; inicio < fim; passo++){
-        //       divResultado.innerHTML=`${fim}`
-        //  }
-    }
-    else{
-        window.alert('Fim menor que inicio')
+    if(formInicio.value.length == 0 || formFim.value.length == 0 || formPasso.value.length == 0)
+    {
+      divResultado.innerHTML ='Impossível contar'
+    }else{
+      divResultado.innerHTML = 'Contando: <br>'
+      var inicio = Number(formInicio.value)
+      var fim = Number(formFim.value)
+      var passo= Number(formPasso.value)
+     
+      if(passo <= 0){
+        window.alert('Passo Inválido. Considerando Passo = 1')
+        passo = 1
+      }
+     
+      if(inicio < fim){//Crescente 
+        for(var i = inicio; i <= fim; i+=passo){
+          divResultado.innerHTML +=` ${i} \u{1F449} `
+        }
+      }else {//Regressiva
+        for(var i = inicio; i >= fim; i-=passo){
+          divResultado.innerHTML +=` ${i} \u{1F449} `
+        }
+      }
+      divResultado.innerHTML +=`\u{1F3C1} `
+      
     }
 }
