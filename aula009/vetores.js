f6f93f216fef6debca2bef367c9363a879eb2b39
Geilso Faria <109383516+GeilsoFaria@users.noreply.github.com>
2024-03-06 18:59:41 -0400

    pop() em JS

--- aula009/vetores.js
+++ aula009/vetores.js
@@ -1,9 +1,12 @@
 var vet = [5,8,2,9,3]//Variável composta(Vetor)
 //vet[5]= 20//Acrecentando manualmente na última posição
 vet.push(1)//Acrescentando automaticamente na última posição
+
 vet.sort()//Ordena vetor (crescente)
 var posicao = vet.indexOf(1)//Procura elementos e retorna indice se tiver, senão -1
 
+console.log(vet)
+vet.pop()//Remover último elemento
 console.log(vet)
 console.log(`O vetor tem ${vet.length} posições`)
 
